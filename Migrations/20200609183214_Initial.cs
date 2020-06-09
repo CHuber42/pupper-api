09b55fc25ccdd4686cc4b7dc0564af218fac8d6e
mai cook <mailiecook@yahoo.com>
2020-06-09 14:46:03 -0700

    add migrations, attempt adding cors

---
+++ Migrations/20200609183214_Initial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pupper.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doggos",
+                columns: table => new
+                {
+                    DoggoId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Breed = table.Column<string>(maxLength: 20, nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    PicUrl = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doggos", x => x.DoggoId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Doggos",
+                columns: new[] { "DoggoId", "Age", "Breed", "Gender", "Name", "PicUrl" },
+                values: new object[] { 1, 7, "Mutt", "Female", "Matilda", "https://t1.ea.ltmcdn.com/en/images/4/0/0/small_white_dog_breeds_3004_600.jpg" });
+
+            migrationBuilder.InsertData(
+                table: "Doggos",
+                columns: new[] { "DoggoId", "Age", "Breed", "Gender", "Name", "PicUrl" },
+                values: new object[] { 2, 11, "Dachshund", "Male", "Bagera", "https://i.pinimg.com/originals/0c/dc/6b/0cdc6b9ea47cdee8ff88c1d6c9e02ca7.jpg" });
+
+            migrationBuilder.InsertData(
+                table: "Doggos",
+                columns: new[] { "DoggoId", "Age", "Breed", "Gender", "Name", "PicUrl" },
+                values: new object[] { 3, 2, "Mutt", "Male", "Flash", "https://www.sciencesource.com/Doc/TR1_WATERMARKED/0/f/c/f/SS2695389.jpg?d63644087851" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doggos");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,48 +12,60 @@ using Pupper.Models;
 namespace Pupper
 {
   public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<PupperContext>(opt =>
-                opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
-
-            services.AddSwaggerGen(c =>
-             {       
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseSwagger();
-             app.UseSwaggerUI(c =>
-        {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-         c.RoutePrefix = string.Empty;
-        });
-            // app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<PupperContext>(opt =>
+          opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+
+      services.AddSwaggerGen(c =>
+       {
+         c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+       });
+
+      services.AddCors(options =>
+        {
+          options.AddPolicy(name: MyAllowSpecificOrigins,
+            builder =>
+            {
+              builder.WithOrigins("http://localhost:5000");
+            });
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+ {
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+   c.RoutePrefix = string.Empty;
+ });
+      // app.UseHttpsRedirection();
+      app.UseCors(MyAllowSpecificOrigins);
+      app.UseMvc();
+    }
+  }
 }
